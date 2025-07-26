dae0ed99f89ea66d98ac228c47bea62a71a607ad
MrVineetRaj <vineetrajrj26@gmail.com>
2025-07-26 21:53:11 +0530

    Fixed params types

--- src/app/api/components/[component_category]/[component_slug]/route.ts
+++ src/app/api/components/[component_category]/[component_slug]/route.ts
@@ -1,12 +1,13 @@
 import { COMPONENTS_DB } from "@/constant/component-db";
 import { NextRequest, NextResponse } from "next/server";
 
-
 export async function GET(
   req: NextRequest,
-  { params }: { params: { component_category: string; component_slug: string } }
+  {
+    params,
+  }: { params: Promise<{ component_category: string; component_slug: string }> }
 ) {
-  const { component_category, component_slug } = params;
+  const { component_category, component_slug } = await params;
 
   if (component_category in COMPONENTS_DB) {
     if (component_slug in COMPONENTS_DB[component_category]) {
