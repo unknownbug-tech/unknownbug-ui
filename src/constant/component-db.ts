4759e04b34b9661698f577025892553a91186d5a
MrVineetRaj <vineetrajrj26@gmail.com>
2025-07-26 21:15:51 +0530

    test-component

---
+++ src/constant/component-db.ts
@@ -0,0 +1,33 @@
+export interface ComponentManifestJson {
+  name: string;
+  dependencies: string[];
+  registryDependencies: string[];
+  files: { path: string; content: string }[];
+  type: string;
+}
+
+export const COMPONENTS_DB: {
+  [component_category: string]: {
+    [component_slug: string]: ComponentManifestJson;
+  };
+} = {
+  test: {
+    "test-1": {
+      name: "test",
+      dependencies: ["button"],
+      registryDependencies: [],
+      files: [
+        {
+          path: `components/unknownbug-ui/test.tsx`,
+          content: `import { Button } from "@/components/ui/button";
+
+                    export default function Test() {
+                      return <Button>Hello Test</Button>;
+                    }
+`,
+        },
+      ],
+      type: "components:ui",
+    },
+  },
+};
